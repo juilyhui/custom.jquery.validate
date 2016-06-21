dbc3baa67304a70780d50d0e783cf544e82efd05
juily <juily.jjh@qq.com>
2016-06-21 14:50:51 +0800

    hui

--- js/custom.jquery.validate.js
+++ js/custom.jquery.validate.js
@@ -5,24 +5,19 @@
 // 
 
 jQuery.extend($.fn, {
-	// 传入要验证的数组, 和提交按钮的类名, 提示文字时间
-	getForm: function(rules, btn, time) {
+	// 传入要验证的数组, 和提交按钮的类名, 提示文字时间, 表单验证成功执行的函数
+	getForm: function(rules, btn, time, successFun) {
 		// 点击提交
 		$("form").find("."+btn+"").on("click",function(e){
 			e.preventDefault();
-			console.log("THIS : ", this);
-			console.log("THISFORM : ", $(this.form));
 			// 获取到发生点击事件的当前表单
 			var $form = $(this.form);
-			if ($().checkRequired($form, time)){
-				// 如果验证required成功
-				if($().checkForm($form, rules, time)){
-					$().confirmFun($form, time);
-				}
+			if ($().checkRequired($form, time) && $().checkForm($form, rules, time) && $().confirmFun($form, time)){
+				// 表单验证成功执行的函数
+				successFun();
 			} else {
-				console.log("VALIDATE FAIL!!!")
+				console.log("VALIDATE FAIL!!!");
 			}
-			
 		})
 	},
 
@@ -39,8 +34,6 @@ jQuery.extend($.fn, {
 		for (var i = 0; i < form.length; i++){
 			var input = form[i];
 			var required = input.required; 
-			console.log("TEXT : ", $(input)[0].value)
-			console.log(required)
 			if (required && $(input)[0].value == ""){
 				// 含required属性 并且为空的
 				$().clearInfo($form);
@@ -54,18 +47,16 @@ jQuery.extend($.fn, {
 				$form.find("["+input.getAttribute("data-none")+"]").slideUp();
 			}
 		}
-		console.log("STATE : ", state)
 		return state;
 	},
 
 	// 表单规则验证
 	checkForm: function($form, rules, time) {
-		console.log("CHECKFORM", $form)
         var state = true;
         var form = $form.get(0);
         var validObj;
         for(var i =0; i< rules.length; i++) {
-        	validObj = $(form).find("#"+rules[i].name)[0];
+        	validObj = $(form).find("[data-name="+rules[i].name+"]")[0];
         	if(!rules[i].rules.test(validObj.value)){
         		// 不正确
         		$().clearInfo($form);
@@ -85,12 +76,11 @@ jQuery.extend($.fn, {
 
 	// 再次输入密码之类的验证
 	confirmFun: function($form, time) {
-		console.log("CONFIRM : ", $form);
 		var state = true;
 		var form = $form.get(0);
 		var confirmObj = $form.find("input[data-confirm]"); // 数组 
 		var confirmTarget;
-		console.log("CONFIRMOBJ : ", confirmObj);
+		// console.log("CONFIRMOBJ : ", confirmObj);
 		for(var i= 0; i< confirmObj.length; i++) {
 			confirmTarget = $(confirmObj[i]).attr("data-confirm").split("-")[1];
 			if($form.find("input[data-rules = "+confirmTarget+"]")[0].value != $(confirmObj[i])[0].value){
@@ -113,26 +103,5 @@ jQuery.extend($.fn, {
 })
 
 
-// // 自定义验证的东西和验证规则
-// // --name-- 即id名
-// // --rules-- 即验证规则，正则
-// var signRules = [
-// 		{
-// 			"id" : "0",
-// 			"name" : "usernameValid",
-// 			"rules" : /^[\u4e00-\u9fa50-9A-Za-z_-]{2,20}$/,
-// 		},
-// 		{
-// 			"id" : "1",
-// 			"name" : "emailValid",
-// 			"rules" : /^[0-9a-zA-Z]+@([0-9a-zA-Z]){2,20}$/,
-// 		},
-// 		{
-// 			"id" : "2",
-// 			"name" : "passwordValid",
-// 			"rules" : /^[0-9A-Za-z]{6,20}$/,
-// 		}
-// ];
-// // 调用函数，传入规则数组，提交按钮类名，和提示文案时间
-// $().getForm(signRules, "submitBtn", "10000");
+
 	
